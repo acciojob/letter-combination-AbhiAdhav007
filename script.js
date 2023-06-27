67b052b6e708351a7417a0cdb121e8fcf8b0236f
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-06-27 13:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-let res = [];
+function letterCombinations(input) {
+  //Complete the function
 
-function letterCombinations(input_digit) {
-  let arr = [0, 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
-  helper(arr, input_digit, res, 0, '');
-  return res.sort((a, b) => a.length - b.length || a.localeCompare(b));
-}
+	let table={
+		"2": "abc",
+		"3": "def",
+		"4": "ghi",
+		"5": "jkl",
+		"6": "mno",
+		"7": "pqrs",
+		"8": "tuv",
+		"9": "wxyz"
+	}
 
-function helper(arr, input_digit, res, index, str) {
-  if (str.length === input_digit.length) {
-    res.push(str);
-    return;
-  }
-  
-  if (index === input_digit.length) {
-    return;
-  }
-  
-  let match = parseInt(input_digit[index]);
-  let ss = arr[match];
-  
-  for (let i = 0; i < ss.length; i++) {
-    str = str + ss[i];
-    helper(arr, input_digit, res, index + 1, str);
-    str = str.substring(0, str.length - 1);
-  }
+	let solution=[];
+
+	function solve(currentIndex, calculatedString){
+		if(input.length<= currentIndex){
+			solution.push(calculatedString);
+			return;
+		}
+
+		let currentChar = input[currentIndex];
+		for(let i=0;i< table[currentChar].length;i++){
+			solve(currentIndex +1, calculatedString + table[currentChar][i]);
+		}
+	}
+	solve(0, "");
+
+	return(solution);
+
+	
 }
 
 module.exports = letterCombinations;
