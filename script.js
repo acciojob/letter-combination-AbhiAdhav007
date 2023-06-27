f5b63cd5a4839dc59a0f6e82aeaf557c13328965
AbhiAdhav007 <120732315+AbhiAdhav007@users.noreply.github.com>
2023-06-27 13:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+function letterCombinations(digits) {
+  const digitToLetters = {
+    '0': '0',
+    '1': '1',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
 
-module.exports = letterCombinations;
+  const combinations = [];
+
+  if (digits.length === 0) {
+    return combinations;
+  }
+
+  backtrack('', digits);
+
+  return combinations;
+
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      combinations.push(combination);
+      return;
+    }
+
+    const currentDigit = nextDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(combination + letter, nextDigits.slice(1));
+    }
+  }
+}
